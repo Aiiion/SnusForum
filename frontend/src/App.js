c1cb554d6324e8c4dfaca357e9485801313bf1b3
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-05-10 10:58:52 +0200

    Add profile and home component. > > Co-authored-by: Anna anna.paajarvi@chasacademy.se

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from './components/Login';
 import Register from './components/Register';
+import Profile from './components/Profile';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Login />
       <Register />
+      {/* <Profile />  */}
     </div>
   );
 }
