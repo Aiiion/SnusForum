a09a3522fc9bf5f08da0428b3f79592f9a6cd1b2
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-05-10 10:53:33 +0200

    Add register component. > > Co-authored-by: Anna anna.paajarvi@chasacademy.se

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from './components/Login';
+import Register from './components/Register';
 
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Register />
     </div>
   );
 }
