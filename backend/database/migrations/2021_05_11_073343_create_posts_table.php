b7bff3bbe47cf3257397dd0af9f1d3a54b9c17a6
ottoreimers <otto.reimers@gmail.com>
2021-05-11 09:43:43 +0200

    added tables for posts, categorys and comments

--- backend/database/migrations/2021_05_11_073343_create_posts_table.php
+++ backend/database/migrations/2021_05_11_073343_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReviewsTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateReviewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('reviews', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('users_id');
-            $table->foreignId('snus_id');
             $table->string('title');
             $table->longText('body');
-            $table->integer('rating');
+            $table->foreignId('users_id');
+            $table->foreignId('categorys_id');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateReviewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reviews');
+        Schema::dropIfExists('posts');
     }
 }
--- backend/database/migrations/2021_05_11_073705_create_comments_table.php
+++ backend/database/migrations/2021_05_11_073705_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReviewsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateReviewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('reviews', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('posts_id');
             $table->foreignId('users_id');
-            $table->foreignId('snus_id');
-            $table->string('title');
             $table->longText('body');
-            $table->integer('rating');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateReviewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reviews');
+        Schema::dropIfExists('comments');
     }
 }
