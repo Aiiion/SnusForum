8e0767554fe2e73e3cdc847e40e662625d6688f2
Alex <alex.soderberg97@gmail.com>
2021-05-11 13:49:52 +0200

    fixes seeders

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('snus_id');
+            $table->foreignId('snus_id')->nullable();
             $table->boolean('user_admin');
             $table->rememberToken();
             $table->timestamps();
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,10 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
-         \App\Models\Flavour::factory(10)->create();
+        \App\Models\Flavour::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Reviews::factory(10)->create();
+         
+         
          \App\Models\Snus::factory(10)->create();
-         \App\Models\Reviews::factory(10)->create();
+         
 
     }
 }
