0dcfbb4cd54fe69f730c9e59b62b1fd4cae0ff3a
Alex <alex.soderberg97@gmail.com>
2021-05-11 15:30:51 +0200

    minor fix

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,9 @@
 <?php
-​
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-​
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,11 +14,11 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-​
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
-​
+
 ], function ($router) {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
