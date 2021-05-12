710834bd7ca5d6f2f7f4d9e44a79051c0b5180c8
woxer <woxer123@gmail.com>
2021-05-12 14:12:39 +0200

    footer update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Link } from "react-router-dom";
 import './App.css';
 
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { Footer } from "./components/Footer";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import { ReactComponent as Logo } from "./logo.svg";
 
 import AuthService from "./services/auth.service";
@@ -110,6 +110,131 @@ const App = () => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+
+          <MDBFooter color="stylish-color-dark" className="page-footer font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="text-uppercase mb-4 mt-3 font-weight-bold">
+              Footer Content
+            </h5>
+            <p>
+              Here you can use rows and columns here to organize your footer
+              content. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit.
+            </p>
+          </MDBCol>
+          <hr className="clearfix w-100 d-md-none" />
+          <MDBCol md="2">
+            <h5 className="text-uppercase mb-4 mt-3 font-weight-bold">
+              Links
+            </h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!">Link 1</a>
+              </li>
+              <li>
+                <a href="#!">Link 2</a>
+              </li>
+              <li>
+                <a href="#!">Link 3</a>
+              </li>
+              <li>
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+          <hr className="clearfix w-100 d-md-none" />
+          <MDBCol md="2">
+            <h5 className="text-uppercase mb-4 mt-3 font-weight-bold">
+              Links
+            </h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!">Link 1</a>
+              </li>
+              <li>
+                <a href="#!">Link 2</a>
+              </li>
+              <li>
+                <a href="#!">Link 3</a>
+              </li>
+              <li>
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+          <hr className="clearfix w-100 d-md-none" />
+          <MDBCol md="2">
+            <h5 className="text-uppercase mb-4 mt-3 font-weight-bold">
+              Links
+            </h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!">Link 1</a>
+              </li>
+              <li>
+                <a href="#!">Link 2</a>
+              </li>
+              <li>
+                <a href="#!">Link 3</a>
+              </li>
+              <li>
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <hr />
+      <div className="text-center py-3">
+        <ul className="list-unstyled list-inline mb-0">
+          <li className="list-inline-item">
+            <h5 className="mb-1">Register for free</h5>
+          </li>
+          <li className="list-inline-item">
+            <a href="#!" className="btn btn-danger btn-rounded">
+              Sign up!
+            </a>
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <div className="text-center">
+        <ul className="list-unstyled list-inline">
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-fb mx-1">
+              <i className="fab fa-facebook-f"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-tw mx-1">
+              <i className="fab fa-twitter"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-gplus mx-1">
+              <i className="fab fa-google-plus"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-li mx-1">
+              <i className="fab fa-linkedin-in"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-dribbble mx-1">
+              <i className="fab fa-dribbble"> </i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
 
       <div className="container mt-3">
         <Switch>
