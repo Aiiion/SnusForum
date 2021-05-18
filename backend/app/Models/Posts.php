a1726ca80ac5712ec31715b7ea3aa7be167ba9b1
Popa <emapopa1995@gmail.com>
2021-05-18 11:20:03 +0200

    changed posts model

--- backend/app/Models/Posts.php
+++ backend/app/Models/Posts.php
@@ -8,8 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Posts extends Model
 {
     use HasFactory;
-    public function categorys(){
-        return $this->hasOne('\App\Models\Categorys')->get();
+
+    public function user(){
+        return $this->belongsTo('\App\Models\User')->first();
+    }
+
+
+    public function category(){
+        return $this->belongsTo('\App\Models\Categorys')->first();
     }
 
     public function comments(){
