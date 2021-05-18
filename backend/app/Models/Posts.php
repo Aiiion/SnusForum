94d83a44b1fc8561c019eaf141185866d74e60fa
Popa <emapopa1995@gmail.com>
2021-05-18 09:16:34 +0200

    fixed Post model

--- backend/app/Models/Posts.php
+++ backend/app/Models/Posts.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Posts extends Model
 {
     use HasFactory;
+    public function categorys(){
+        return $this->hasOne('\App\Models\Categorys')->get();
+    }
+
+    public function comments(){
+        return $this->hasMany('\App\Models\Comments')->get();
+    }
+
 }
