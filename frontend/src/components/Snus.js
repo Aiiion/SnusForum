574b33878b511cffb3c7f6ed790de710476067fc
Anna Paajarvi <anna.paajarvi@chasacademy.se>
2021-05-19 10:40:10 +0200

    Add snus component and fix get requests from db. > > Co-authored-by: Pernilla pernilla.hallgren@chasacademy.se

--- frontend/src/components/Snus.js
+++ frontend/src/components/Snus.js
@@ -1,22 +1,19 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Switch, Route, Link } from "react-router-dom";
+import authHeader from "../services/auth-header";
 
-import { Card, ListGroup, ListGroupItem, Form, FormControl, Button, Container, CardGroup } from "react-bootstrap";
+import { Card, ListGroup, ListGroupItem, Form, FormControl, Button, Container, CardGroup, Row, Col } from "react-bootstrap";
 import * as Icon from 'react-bootstrap-icons';
 
-import SnusService from "../services/snus.service";
 import SnusReview from "./SnusReviews";
-import zyn from "../image/zyn-snus.png";
-
-import UserService from "../services/user.service";
-import axios from "axios";
 
 const Snus = () => {
 
     const [snus, setSnus] = useState("");
 
     useEffect(() => {
-        axios.get('https://snusare-backend.herokuapp.com/api/auth/snuses')
+        axios.get('https://snusare-backend.herokuapp.com/api/auth/snuses', { headers: authHeader() })
             .then(response => {
                 // JSON responses are automatically parsed.
                 setSnus(response.data)
@@ -38,102 +35,42 @@ const Snus = () => {
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                 <Button className="mt-3 mb-3" variant="outline-success">Sök snus</Button>
             </Form>
+
             <Container>
                 <CardGroup>
-                    <Card className="m-3">
-                        <Card.Title>{snus.snuses[0].name}</Card.Title>
-                        <Card.Body style={{ backgroundColor: '#F2F3F8' }}>
-                            <Card.Img variant="top" src={zyn} />
-                        </Card.Body>
-                        <Card.Text>
-                            Smak av persika med inslag av fruktte och grön druva. En liten tunn nikotinpåse som är torr men som när den fuktats upp under läppen ger en snabb och kraftig smakleverans.
-                        </Card.Text>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>Styrka: {snus.snuses[0].strength}</ListGroupItem>
-                            <ListGroupItem>Typ: {snus.snuses[0].type}</ListGroupItem>
-                            <ListGroupItem>Format: Mini</ListGroupItem>
-                            <ListGroupItem>Smak: Frukt & Bär</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                            <Card.Link href="#"><Icon.StarFill style={{ fill: '#8E92A4' }}></Icon.StarFill></Card.Link>
-                            <Card.Link href="/snus-review"><Icon.ChatLeftTextFill style={{ fill: '#8E92A4' }}></Icon.ChatLeftTextFill></Card.Link>
-                        </Card.Body>
-                    </Card>
+                    <Row>
+                        {snus.snuses.map((snus) => (
+                            <Col sm="6" md="4" lg="4" >
+                                <Card>
 
-                    <Card className="m-3">
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Body style={{ backgroundColor: '#F2F3F8' }}>
-                            <Card.Img variant="top" src={zyn} />
-                        </Card.Body>
-                        <Card.Text>
-                            Smak av persika med inslag av fruktte och grön druva. En liten tunn nikotinpåse som är torr men som när den fuktats upp under läppen ger en snabb och kraftig smakleverans.
-                        </Card.Text>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>Styrka: Normal</ListGroupItem>
-                            <ListGroupItem>Typ: Nikotinpåsar</ListGroupItem>
-                            <ListGroupItem>Format: Mini</ListGroupItem>
-                            <ListGroupItem>Smak: Frukt & Bär</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                            <Card.Link href="#"><Icon.StarFill style={{ fill: '#8E92A4' }}></Icon.StarFill></Card.Link>
-                            <Card.Link href="/snus-review"><Icon.ChatLeftTextFill style={{ fill: '#8E92A4' }}></Icon.ChatLeftTextFill></Card.Link>
-                        </Card.Body>
-                    </Card>
-
-                    <Card className="m-3">
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Body style={{ backgroundColor: '#F2F3F8' }}>
-                            <Card.Img variant="top" src={zyn} />
-                        </Card.Body>
-                        <Card.Text>
-                            Smak av persika med inslag av fruktte och grön druva. En liten tunn nikotinpåse som är torr men som när den fuktats upp under läppen ger en snabb och kraftig smakleverans.
-                        </Card.Text>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>Styrka: Normal</ListGroupItem>
-                            <ListGroupItem>Typ: Nikotinpåsar</ListGroupItem>
-                            <ListGroupItem>Format: Mini</ListGroupItem>
-                            <ListGroupItem>Smak: Frukt & Bär</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                            <Card.Link href="#"><Icon.StarFill style={{ fill: '#8E92A4' }}></Icon.StarFill></Card.Link>
-                            <Card.Link href="/snus-review"><Icon.ChatLeftTextFill style={{ fill: '#8E92A4' }}></Icon.ChatLeftTextFill></Card.Link>
-                        </Card.Body>
-                    </Card>
-
-                    <Card className="m-3">
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Body style={{ backgroundColor: '#F2F3F8' }}>
-                            <Card.Img variant="top" src={zyn} />
-                        </Card.Body>
-                        <Card.Text>
-                            Smak av persika med inslag av fruktte och grön druva. En liten tunn nikotinpåse som är torr men som när den fuktats upp under läppen ger en snabb och kraftig smakleverans.
-                        </Card.Text>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>Styrka: Normal</ListGroupItem>
-                            <ListGroupItem>Typ: Nikotinpåsar</ListGroupItem>
-                            <ListGroupItem>Format: Mini</ListGroupItem>
-                            <ListGroupItem>Smak: Frukt & Bär</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                            <Card.Link href="#"><Icon.StarFill style={{ fill: '#8E92A4' }}></Icon.StarFill></Card.Link>
-                            <Card.Link href="/snus-review"><Icon.ChatLeftTextFill style={{ fill: '#8E92A4' }}></Icon.ChatLeftTextFill></Card.Link>
-                        </Card.Body>
-                    </Card>
-
+                                    <Card.Body style={{ backgroundColor: '#F2F3F8' }}>
+                                        <Card.Img variant="top" src={snus.img_url} />
+                                    </Card.Body>
+                                    <Card.Title>{snus.name}</Card.Title>
+                                    <ListGroup className="list-group-flush">
+                                        <ListGroupItem>Styrka: {snus.strength}</ListGroupItem>
+                                        <ListGroupItem>Typ: {snus.type}</ListGroupItem>
+                                        {/* <ListGroupItem>Format: </ListGroupItem> */}
+                                        <ListGroupItem>Smak: {snus.flavour_id}</ListGroupItem>
+                                    </ListGroup>
+                                    <Card.Body>
+                                        <Card.Link href="#"><Icon.StarFill style={{ fill: '#8E92A4', float: 'left' }}></Icon.StarFill></Card.Link>
+                                        <Card.Link href="/snus-review"><Icon.ChatLeftTextFill style={{ fill: '#8E92A4', float: 'right' }}></Icon.ChatLeftTextFill></Card.Link>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))};
+                    </Row>
                 </CardGroup>
             </Container>
-
 
             <div>
                 <Switch>
                     <Route exact path={["/snus-review"]} component={SnusReview} />
                 </Switch>
             </div>
-
         </>
-
         : null
-
 }
 
 export default Snus;
