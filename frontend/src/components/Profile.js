3263782148bb8ba59d6bf3db2075df53b5b16daf
Popa <emapopa1995@gmail.com>
2021-05-26 16:42:32 +0200

    edited profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,15 +1,21 @@
 import React from "react";
 import AuthService from "../services/auth.service";
+import { Card, ListGroup, ListGroupItem, Form, FormControl, Button, Container, CardGroup, Row, Col, Image} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 const Profile = () => {
   const currentUser = AuthService.getCurrentUser();
   console.log(currentUser);
 
   return (
+
+    <>
     <div className="container">
       <header className="jumbotron">
+      <Image src="src/imgage/tobac.png"  />
         <h3>
-          <strong>Profile:</strong> {currentUser.user.username} 
+          <strong>Welcome:</strong> {currentUser.user.username} 
         </h3>
       </header>
       {/* <p>
@@ -28,6 +34,42 @@ const Profile = () => {
           currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
       </ul> */}
     </div>
+    <CardGroup>
+    <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>Post</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">View your Posts</Card.Subtitle>
+    <Card.Text>
+      When you click on the link you will be able to see your older posts.
+    </Card.Text>
+    <Card.Link href="#">Show Posts</Card.Link>
+  </Card.Body>
+</Card>
+
+<Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>Reviews</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">View your Reviews</Card.Subtitle>
+    <Card.Text>
+    When you click on the link you will be able to see your older reviews.
+    </Card.Text>
+    <Card.Link href="#">Show Reviews</Card.Link>
+  </Card.Body>
+</Card>
+
+<Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>Comments</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">View your Comments</Card.Subtitle>
+    <Card.Text>
+      SWhen you click on the link you will be able to see your older comments.
+    </Card.Text>
+    <Card.Link href="#">Show Comments</Card.Link>
+  </Card.Body>
+</Card>
+</CardGroup>
+    </>
+  
   );
 };
 
