e63776cf189344f7775faf666fbbeb72c53484b1
Braggedtooth <54339202+Braggedtooth@users.noreply.github.com>
2021-05-28 13:18:39 +0200

    Update store-favourites.js

--- frontend/src/services/store-favourites.js
+++ frontend/src/services/store-favourites.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-import { getCurrentUser } from "./auth.service";
 
 const API_URL = "https://snusare-backend.herokuapp.com/api/auth/";
-const currentuser = getCurrentUser()
+
 
 export default function saveFavourite(snusID) {
     let bodyFormData = new FormData();
     bodyFormData.append('flavours_id', snusID);
-    bodyFormData.append('users_id', currentuser.user.id);
+
 
 
     return axios({
